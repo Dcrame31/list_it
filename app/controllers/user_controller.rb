f3d3fc8c5f346904b35c70c5f0523d5044bfce79
Diane Crame <dcrame@icloud.com>
2020-05-11 16:42:05 -0700

    added routes to user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,9 +2,27 @@ class UserController < ApplicationController
 
     get '/home' do
         if logged_in?
+            @user = current_user
             erb :'users/index'
         else
             redirect '/'
         end
     end
+
+    get '/signup' do
+        
+    end
+
+    post '/signup' do
+        
+    end
+
+    get '/login' do
+        
+    end
+
+    post '/login' do
+        
+    end
+
 end
