772b517dce1ed962aa27674a953667bced332256
Diane Crame <dcrame@icloud.com>
2020-05-14 12:40:27 -0700

    update config.ru and lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,26 @@
-class ListController < ApplicationController
+class ListsController < ApplicationController
+
+    get '/lists' do
+        @lists = List.all
+        if logged_in?
+            @user = current_user
+            erb :'lists/index'
+        else
+            redirect '/login'
+        end 
+    end
+
+    get '/lists/new' do
+        if logged_in?
+            erb :'lists/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/lists' do
+        
+    end
+
 
 end
