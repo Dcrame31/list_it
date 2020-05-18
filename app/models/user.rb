8551d3f56307cd9c57a6e75d8a271b878b42f8cd
Diane Crame <dcrame@icloud.com>
2020-05-18 13:47:19 -0700

    fixed validation bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :lists
     has_many :categories, through: :lists
-    validates_uniqueness_of :username
+    validates :username, :presence => true, :uniqueness => true
+
 end
